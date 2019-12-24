8cba7f4b98828d00bb5865d15e448a40336b214d
ישראל רולניק < @DESKTOP-CH145OH>
2019-12-24 21:37:59 +0200

    Add project files.

---
+++ data_structer_3/lending.h
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+class Lending
+{
+	int id;
+	string name;
+	string date;
+	int itemCode;
+public:
+	Lending(){}
+	Lending(int, string, string, int);
+	~Lending() {}
+	void set_date(string);
+	bool operator==(Lending a)const;
+	bool operator!=(Lending a) { return!operator==(a); }
+	bool operator<(Lending a);
+	bool operator>(Lending a);
+	bool operator<=(Lending a);
+	bool operator>=(Lending a);
+	int getYear() { return atoi(date.substr(0, 4).c_str()); }
+	int getMonth() { return atoi(date.substr(6, 2).c_str()); }
+	int getDay() { return atoi(date.substr(9, 2).c_str()); }
+	friend istream& operator>>(istream& is, Lending& l)
+	{
+		cout << "enter: id, name, date, item ";
+		is >> l.id >> l.name >> l.date >> l.itemCode;
+		return is;
+	}
+	friend ostream& operator<<(ostream& os, Lending& l)
+	{
+		os << endl << "Lending details: id: " << l.id << " name: " << l.name << " date: " << l.date << " item: " << l.itemCode << endl;
+		return os;
+	}
+};
+bool Lending::operator==(Lending a)const
+{
+	return id == a.id && name == a.name && date == a.date && itemCode == a.itemCode;
+}
+bool Lending::operator<(Lending a)
+{
+	if (operator==(a))
+		return false;
+	else if (getYear() < getYear())
+		return true;
+	else if (getMonth() < getMonth() && getYear() == getYear())
+		return true;
+	else if (getDay() < getDay() && getMonth() == getMonth() && getYear() == getYear())
+		return true;
+	else if (date == a.date && itemCode < a.itemCode)
+		return true;
+	else if (id < a.id)
+		return true;
+	else return false;
+}
+bool Lending::operator<=(Lending a)
+{
+	return operator<(a) || operator==(a);
+}
+bool Lending::operator>(Lending a)
+{
+	return !operator<(a) && !operator==(a);
+}
+bool Lending::operator>=(Lending a)
+{
+	return operator>(a) || operator==(a);
+}
+Lending::Lending(int a,string b,string c,int d)
+{
+	id = a; name = b; date = c; itemCode = d;
+}
---
+++ data_structer_3/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <vector>
+#include <functional>
+#include "BTree.h"
+#include "Lending.h"
+
+using namespace std;
+
+void main()
+{
+	char ch;
+	BTree<Lending> bt;
+	vector<string> ids;
+	Lending l1, l2;
+	int id, itemCode;
+	string str, str1, str2, name, date;
+	vector<Lending> keys;
+	Lending* p;
+	
+
+	cout << "\nChoose one of the following" << endl;
+	cout << "1: add a new lending" << endl;
+	cout << "2: return an item" << endl;
+	cout << "3: print all lendings " << endl;
+	cout << "4: print all lendings of a date " << endl;
+	cout << "5: exit:" << endl;
+	do
+	{
+		cin >> ch;
+
+		switch (ch)
+		{
+		case '1':
+			cout << "Enter lending data\n";
+			//cin >> l1;
+			bt.insert(l1);
+			break;
+
+		case '2':
+			cout << "Enter lending data\n";
+			//cin >> l1;
+			bt.deleteVal(l1);
+			break;
+		case '3':
+			bt.printTree();
+			break;
+		case '4':cout << "Enter the required date\n";
+			cin >> str;
+			str1 = str.substr(0, str.length() - 1) + '*'; //before date
+			str2 = str + '*'; //after date
+			l1.set_date(str1);
+			l2.set_date(str2);
+			bt.printBetween(l1, l2);
+			break;
+		case '5':cout << "bye "; break;
+		default: cout << "error ";  break;
+		}//switch
+	} while (ch != '5');
+
+}
